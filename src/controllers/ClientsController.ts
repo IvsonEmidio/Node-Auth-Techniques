a7a39f00243f7c3503b58f8aa734f0b4046280da
IvsonEmidio <contato.ivsonemidio@gmail.com>
2022-04-19 03:00:12 -0300

    Updated faker data

--- src/controllers/ClientsController.ts
+++ src/controllers/ClientsController.ts
@@ -7,7 +7,14 @@ export class ClientsController {
         id: 7,
         name: "Ivson Emidio",
         age: 22,
+        created_at: new Date('2000-03-27')
       },
+      {
+        id: 8,
+        name: "The world alive",
+        age: 44,
+        created_at: new Date('2010-12-11')
+      }
     ]);
   }
 }
